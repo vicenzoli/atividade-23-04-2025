11d94c213022d26ebc979f521f7476432bae8c13
VICENZO PEREIRA LIBRELOTTO <vicenzo_librelotto@estudante.sesisenai.org.br>
2025-04-23 09:41:02 -0300

    terceirocommit

--- ApiRestful-dev/fs.module.js
+++ ApiRestful-dev/fs.module.js
@@ -1,11 +1,14 @@
 const fs = require ('fs')
 
-console.log('antes')
-try{
-    const data = fs.readFileSync('./ApiRestful-dev/teste.txt','utf-8')
-    console.log(data)
-} catch (e) {
-    console.log(e)
-}
+const texto = 'davi brito'
+fs.writeFile('./ApiRestful-dev/teste.txt', texto, (err) => {
+    if (err){
+    console.log(err)
+    }
+})
 
-console.log('depois')
+fs.appendFile('./ApiRestful-dev/teste.txt', texto, (err) => {
+    if (err){
+    console.log(err)
+    }
+})
